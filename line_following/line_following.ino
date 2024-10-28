f3b90397517b481316b86b5ec42d6fcee1b893db
Sohardtofind <jx321@cam.ac.uk>
2024-10-28 21:08:10 +0000

    low level logic updated

--- line_following/line_following.ino
+++ line_following/line_following.ino
@@ -24,7 +24,7 @@ int enl = 5;  // enable pin for left motor (PWM)
 int vspeed = 100;  // variable speed for forward motion
 int tspeed = 255;  // turning speed
 int tdelay = 20;   // delay during turns
-int currentFacing = 1;    //1 - north   2 - west   3 - south   4 - east
+int current_facing = 1;    //1 - north   2 - west   3 - south   4 - east
 
 void forward()
 {
@@ -33,6 +33,8 @@ void forward()
   leftMotor->run(BACKWARD);
   rightMotor->run(BACKWARD);
 }
+
+// infinitesimal swing turn to the right
 void right()
 {
   leftMotor->setSpeed(150);
@@ -41,6 +43,18 @@ void right()
   rightMotor->run(RELEASE);
   delay(tdelay);
 }
+
+// infinitesimal point turn to the right
+void pointRight()
+{
+  leftMotor->setSpeed(150);
+  rightMotor->setSpeed(150);
+  leftMotor->run(FORWARD);
+  rightMotor->run(BACKWARD);
+  delay(tdelay);
+}
+
+// infinitesimal swing turn to the left
 void left()
 {
   leftMotor->setSpeed(150);
@@ -49,6 +63,17 @@ void left()
   rightMotor->run(FORWARD);
   delay(tdelay);
 }
+
+// infinitesimal point turn to the left
+void pointLeft()
+{
+  leftMotor->setSpeed(150);
+  rightMotor->setSpeed(150);
+  leftMotor->run(BACKWARD);
+  rightMotor->run(FORWARD);
+  delay(tdelay);
+}
+
 void stop()
 {
   leftMotor->setSpeed(150);
@@ -56,122 +81,8 @@ void stop()
   leftMotor->run(RELEASE);
   rightMotor->run(RELEASE);
 }
-void turnRight() {
-  // Set the motors to turn right
-  /*leftMotor->setSpeed(150);
-  rightMotor->setSpeed(150);
-  leftMotor->run(FORWARD);  // Left motor forward
-  rightMotor->run(BACKWARD); // Right motor backward
-  */
-  // Go forward a bit before turning
-  // forward();
-  // The value needs to be tuned by experiments
-  // delay(200);
-  // Ignore the initial high reading (sensor on the line)
-  while (digitalRead(10) == HIGH) {
-    // Wait until sensor no longer detects the line (goes low)
-	right();
-  }
 
-  // Turn right until the sensor detects a line again (becomes high)
-  while (digitalRead(10) == LOW) {
-    // Keep turning right until a line is detected
-	right();
-  }
-
-  if (digitalRead(10) == HIGH)
-  {
-  // Stop turning when another line is found
-  stop();
-  }
-
-  // update facing
-  current_facing = current_facing - 1;
-  if (current_facing == 0)
-  {
-    current_facing = 4;
-  }
-}
-
-void turnLeft(){
-// Set the motors to turn left
-  /*leftMotor->setSpeed(150);
-  rightMotor->setSpeed(150);
-  leftMotor->run(BACKWARD);  // Left motor forward
-  rightMotor->run(FORWARD); // Right motor backward
-  */
-
-  // Go forward a bit before turning
-  // forward();
-  // The value needs to be tuned by experiments
-  // delay(200);
-  // Ignore the initial high reading (sensor on the line)
-  while (digitalRead(10) == HIGH) {
-    // Wait until sensor no longer detects the line (goes low)
-	left();
-  }
-
-  // Turn right until the sensor detects a line again (becomes high)
-  while (digitalRead(10) == LOW) {
-    // Keep turning right until a line is detected
-	left();
-  }
-
-  if (digitalRead(10) == HIGH)
-  {
-  // Stop turning when another line is found
-  	stop();
-  }
-  
-  // update the facing
-  current_facing = current_facing + 1;
-    if (current_facing == 5)
-    {
-      current_facing = 1;
-    }
-}
-
-// Function for changing current facing to desirable facing
-void adjust_facing(int default_facing)
-{
-  //1 - north
-  //2 - west
-  //3 - south 
-  //4 - east
-
-
-  int facing_difference = current_facing - default_facing;
-  switch (facing_difference)
-  {
-    case -3:
-      turnRight();
-      break;
-    case -2:
-      turnRight();
-      turnRight();
-      break;
-    case -1:
-      turnLeft();
-      break;
-    case 0:
-      break;
-    case 1:
-      turnRight();
-      break;
-    case 2:
-      turnRight();
-      turnRight();
-      break;
-    case 3:
-      turnLeft();
-      break;
-  } 
-  
-  current_facing = default_facing;
-
-}
-
-void line_following(); //does not detect the junctions/turns!!!
+void line_following() //does not detect the junctions/turns!!!
 {
     if (sl_val == LOW && sr_val == LOW)
         {  // Both sensors off the line (on the background)
@@ -191,17 +102,18 @@ void line_following(); //does not detect the junctions/turns!!!
         }
 }
 
-void pull_up_point_turn() //pull up for a forward turn
+// pull up when a node or junction is detected so that back sensors align with the turning point
+void pull_up() //pull up for a forward turn
 {
-  int junctionType // 1 - forward and right, 2 - forward and left, 3 - right and left, 4 - all sides
+  int junctionType; // 1 - forward and right, 2 - forward and left, 3 - right and left, 4 - all sides
     if (fsr_val == HIGH, fsf_val == HIGH, fsl_val == LOW)
-      junctionType = 1;
-    elif (fsr_val == LOW, fsf_val == HIGH, fsl_val == HIGH)
-      junctionType = 2;
-    elif (fsr_val == HIGH, fsf_val == LOW, fsl_val == HIGH)
-      junctionType = 3;
+      {junctionType = 1;}
+    else if (fsr_val == LOW, fsf_val == HIGH, fsl_val == HIGH)
+      {junctionType = 2;}
+    else if (fsr_val == HIGH, fsf_val == LOW, fsl_val == HIGH)
+      {junctionType = 3;}
     else
-    junctionType = 4;
+    {junctionType = 4;}
 
   switch (junctionType)
   {
@@ -240,11 +152,170 @@ void pull_up_point_turn() //pull up for a forward turn
 
 }
 
-void randomtestingfunction()
-{
-  delay(1);
+// This function is for swing turn. !!!A point to consider: we still need pull up for swing turn
+void swingTurnRight() {
+
+  pull_up();
+
+  // Ignore the initial high reading (sensor on the line)
+  while (digitalRead(fsf) == HIGH) {
+    // Wait until sensor no longer detects the line (goes low)
+	right();
+  }
+
+  // Turn right until the sensor detects a line again (becomes high)
+  while (digitalRead(fsf) == LOW) {
+    // Keep turning right until a line is detected
+	right();
+  }
+
+  if (digitalRead(fsf) == HIGH)
+  {
+  // Stop turning when another line is found
+  stop();
+  }
+
+  // update facing
+  current_facing = current_facing - 1;
+  if (current_facing == 0)
+  {
+    current_facing = 4;
+  }
 }
 
+void pointTurnRight() {
+  pull_up();
+
+  // Ignore the initial high reading (sensor on the line)
+  while (digitalRead(fsf) == HIGH) {
+    // Wait until sensor no longer detects the line (goes low)
+	pointRight();
+  }
+
+  // Turn right until the sensor detects a line again (becomes high)
+  while (digitalRead(fsf) == LOW) {
+    // Keep turning right until a line is detected
+	pointRight();
+  }
+
+  if (digitalRead(fsf) == HIGH)
+  {
+  // Stop turning when another line is found
+  stop();
+  }
+
+  // update facing
+  current_facing = current_facing - 1;
+  if (current_facing == 0)
+  {
+    current_facing = 4;
+  }
+}
+
+void swingTurnLeft(){
+  pull_up();
+
+  // Ignore the initial high reading (sensor on the line)
+  while (digitalRead(fsf) == HIGH) {
+  // Wait until sensor no longer detects the line (goes low)
+	left();
+  }
+
+  // Turn right until the sensor detects a line again (becomes high)
+  while (digitalRead(fsf) == LOW) {
+    // Keep turning right until a line is detected
+	left();
+  }
+
+  if (digitalRead(fsf) == HIGH)
+  {
+  // Stop turning when another line is found
+  	stop();
+  }
+  
+  // update the facing
+  current_facing = current_facing + 1;
+    if (current_facing == 5)
+    {
+      current_facing = 1;
+    }
+}
+
+void pointTurnLeft(){
+
+  pull_up();
+
+  // Ignore the initial high reading (sensor on the line)
+  while (digitalRead(fsf) == HIGH) {
+    // Wait until sensor no longer detects the line (goes low)
+	pointLeft();
+  }
+
+  // Turn right until the sensor detects a line again (becomes high)
+  while (digitalRead(fsf) == LOW) {
+    // Keep turning right until a line is detected
+	pointLeft();
+  }
+
+  if (digitalRead(fsf) == HIGH)
+  {
+  // Stop turning when another line is found
+  	stop();
+  }
+  
+  // update the facing
+  current_facing = current_facing + 1;
+    if (current_facing == 5)
+    {
+      current_facing = 1;
+    }
+}
+
+// Function for changing current facing to desirable facing, right now we assume point turn and may be adjusted further!!!
+void adjust_facing(int default_facing)
+{
+  //1 - north
+  //2 - west
+  //3 - south 
+  //4 - east
+
+
+  int facing_difference = current_facing - default_facing;
+  switch (facing_difference)
+  {
+    case -3:
+      pull_up();
+      pointTurnRight();
+      break;
+    case -2:
+      pull_up();
+      pointTurnRight();
+      pointTurnRight();
+      break;
+    case -1:
+      pull_up();
+      pointTurnLeft();
+      break;
+    case 0:
+      break;
+    case 1:
+      pull_up();
+      pointTurnRight();
+      break;
+    case 2:
+      pull_up();
+      pointTurnRight();
+      pointTurnRight(); //!!!!!!!!!!! Watch out for if this works
+      break;
+    case 3:
+      pull_up();
+      pointTurnLeft();
+      break;
+  } 
+  
+  current_facing = default_facing;
+
+}
 
 void setup()
 {
@@ -252,9 +323,9 @@ void setup()
   Serial.println("Adafruit Motorshield v2 - DC Motor test!");
   if (!AFMS.begin())
   {         // create with the default frequency 1.6KHz
-  // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
-    Serial.println("Could not find Motor Shield. Check wiring.");
-    while (1);
+    // if (!AFMS.begin(1000)) {  // OR with a different frequency, say 1KHz
+      Serial.println("Could not find Motor Shield. Check wiring.");
+      while (1);
   }
   Serial.println("Motor Shield found.");
   // Set the speed to start, from 0 (off) to 255 (max speed)
@@ -268,6 +339,8 @@ void setup()
   pinMode(11, OUTPUT);
   delay(2000);  // delay for system start-up
 }
+
+// testing: should be able to follow the line, turn at a single turn where there's no junction and finally stop at a junction
 void loop()
 {
   sr_val = digitalRead(sr);  // read right sensor
@@ -278,31 +351,18 @@ void loop()
   digitalWrite(11, LOW);
   if (/*fsf_val == HIGH && */fsr_val == LOW && fsl_val == LOW) // no junction or single turn detected so go straight 
   {
-    if (sl_val == LOW && sr_val == LOW)
-    {  // Both sensors off the line (on the background)
-      forward();  // Move forward
-    }
-    else if (sl_val == HIGH && sr_val == LOW)
-    {  // Left sensor on the line, right sensor off the line
-      right();  // Turn right to move off the line
-    }
-    else if (sl_val == LOW && sr_val == HIGH)
-    {  // Right sensor on the line, left sensor off the line
-      left();  // Turn left to move off the line
-    }
-    else if (sl_val == HIGH && sr_val == HIGH)
-    {  // Both sensors on the line
-      stop();  // Stop or make adjustments
-    }
+    line_following();
   } 
   else if (fsf_val == LOW && fsr_val == HIGH && fsl_val == LOW)  // single right turn
   {
-    turnRight();
+    pull_up();
+    swingTurnRight();
 
   }
   else if (fsf_val == LOW && fsr_val == LOW && fsl_val == HIGH)  // single left turn
   {
-    turnLeft();
+    pull_up();
+    swingTurnLeft();
     
   }
   else                                        // junction detected
@@ -310,8 +370,8 @@ void loop()
     stop();
   }
 
-  {
+  /*{
     digitalWrite(11, HIGH);
-  }
+  }*/
   // delay(20);
 }
